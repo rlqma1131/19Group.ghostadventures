9599bd676206daa44e58d7e4878de52dd827e8d9
DounPark <ehdns1562@gmail.com>
2025-06-30 11:51:44 +0900

    feat: 시계 작성중

--- GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Obect/Ch1_Clock.cs
+++ GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Obect/Ch1_Clock.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ch1_Clock : MonoBehaviour
+public class Ch1_Clock : BasePossessable
 {
+    [SerializeField] private Transform hourHand;
+    [SerializeField] private Transform minuteHand;
+    [SerializeField] private Camera zoomCamera;
+    [SerializeField] private GameObject tvObject;
     
-    
-    void Update()
+    private bool isControlMode = false;
+
+    private int hour = 0;
+    private int minute = 0;
+
+    protected override void Update()
     {
-        
+        base.Update();
+
+        if (!isPossessed)
+        {
+            
+        }
     }
 }
