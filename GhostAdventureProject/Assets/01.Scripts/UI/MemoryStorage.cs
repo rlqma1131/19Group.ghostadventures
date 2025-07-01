ba36e0e68c005bbc5f63f36837b1e9e81ebf3fe1
song-ssi <song33@outlook.kr>
2025-07-01 12:50:34 +0900

    Feat: 기억저장소에 표시될 MemoryData 정보 가져오기

--- GhostAdventureProject/Assets/01.Scripts/UI/MemoryStorage.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/MemoryStorage.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class MemoryStorage : MonoBehaviour
 {
-    [SerializeField] private Transform nodeContainer; // 노드를 담을 부모 오브젝트
+    [SerializeField] private Transform nodeContainer;
     [SerializeField] private GameObject memoryNodePrefab;
+    // [SerializeField] private LineRenderer lineRenderer;
+
+    private List<Transform> nodePositions = new();
 
     private void OnEnable()
     {
         MemoryManager.Instance.OnMemoryCollected += AddMemoryNode;
+        RedrawStorage(); // 초기화 시
     }
 
     private void OnDisable()
@@ -18,9 +22,34 @@ public class MemoryStorage : MonoBehaviour
         MemoryManager.Instance.OnMemoryCollected -= AddMemoryNode;
     }
 
+    private void RedrawStorage()
+    {
+        // 저장된 기억을 기준으로 UI 다시 그림
+        foreach (Transform child in nodeContainer) Destroy(child.gameObject);
+        nodePositions.Clear();
+
+        // 만약 수집된 메모리를 직접 가져올 수 있다면:
+        // foreach (MemoryData memory in MemoryManager.Instance.GetCollectedMemories())
+        // {
+        //     AddMemoryNode(memory);
+        // }
+    }
+
     private void AddMemoryNode(MemoryData memory)
     {
         GameObject node = Instantiate(memoryNodePrefab, nodeContainer);
-        MemoryNode nodeScript = node.GetComponent<MemoryNode>();
-        nodeScript.Initialize(memory);
-    }}
+        node.GetComponent<MemoryNode>().Initialize(memory);
+        nodePositions.Add(node.transform);
+
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        // lineRenderer.positionCount = nodePositions.Count;
+        // for (int i = 0; i < nodePositions.Count; i++)
+        // {
+        //     lineRenderer.SetPosition(i, nodePositions[i].position);
+        // }
+    }
+}
