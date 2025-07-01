0274376609ed3906b5fc63449e3cc345168a0955
hbks0224 <hbks1234@gmail.com>
2025-07-01 15:27:12 +0900

    fix : Cutscene매니저 게임매니저 포함

--- GhostAdventureProject/Assets/01.Scripts/Managers/GameManager.cs
+++ GhostAdventureProject/Assets/01.Scripts/Managers/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private GameObject uiManager;
     [SerializeField] private GameObject PossessionStateManager;
     [SerializeField] private GameObject soundManager;
+    [SerializeField] private GameObject cutSceneManager;
 
     public GameObject playerPrefab;
 
@@ -45,6 +46,7 @@ public class GameManager : Singleton<GameManager>
         EnsureManagerExists<UIManager>(uiManager);
         EnsureManagerExists<PossessionStateManager>(PossessionStateManager);
         EnsureManagerExists<SoundManager>(soundManager);
+        EnsureManagerExists<CutsceneManager>(cutSceneManager);
 
         // 다른 매니저들도 같은 방식으로
         // 추후 스테이지 초기화, UI 초기화 등 여기에 추가
