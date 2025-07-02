a1d6f194a17c5753b3d713576f27a6e2e9c3e3f9
hbks0224 <hbks1234@gmail.com>
2025-07-02 17:28:22 +0900

    feat : new 인트로씬 작업 추가 및 npc 컷신 (임시) 제작

---
+++ GhostAdventureProject/Assets/01.Scripts/TimeLine/teststartScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class teststartScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void introScenestart()
+    {
+
+
+        SceneManager.LoadScene("IntroScene_Real");
+    }
+}
