6d36748a0cbc9b1b1c758303e66fa48762edfeea
unity9Parkjaehyun <game_samson@naver.com>
2025-07-04 00:04:53 +0900

    문 구현
    OpenDoor.cs , LockedDoor.cs , BaseDoor.cs 세 개가 다 있어야 문이 작동합니다!!
    
    모든 문 오브젝트에는 공통적으로 Box Collider 2D 컴포넌트와 Is Trigger 버튼이 체크되어야 합니다 !!!
    
    - 문 근처에서 E키로 상호작용
    
    열려있는 OpenDoor 오브젝트 에는 OpenDoor 스크립트만!!  붙여주세요
    
    Inspector 에서 보시면 두가지 방법으로 문 끼리 이동하도록 구현해놓았습니다.
    
    Target Door 에다가 이동할 문 오브젝트를 드래그해 붙여서 이동하는 방법도 구현해뒀고,
    
    Target Pos 로 , 직접 좌표를 입력에 이동하는 방법도 구현해 뒀으니
    
    둘 중 버그가 안터지거나 편하신 방법으로 구현하시면 됩니다.
    
    ================================================
    
    잠겨있는 LockedDoor 오브젝트에는 LockedDoor.cs 만!! 붙여주세요.
    
    Target Door 에다가 이동할 문 오브젝트를 드래그해 붙여서 이동하는 방법도 구현해뒀고,
    
    Target Pos 로 , 직접 좌표를 입력에 이동하는 방법도 구현해 뒀습니다.
    
    위에서 설명한대로 , Locked Door 도 둘 중 버그가 안터지거나 편하신 방법으로 구현하시면 됩니다.
    
    Puzzle ID 는 작업하시면서 햇갈리지말라고 메모처럼 적어놓는 공간입니다. 그냥 편의성을 제공하기 위해서 만들어놨어요
    
    Locked Sound , UnlockSound 는 문을 건드렸을때 들어갈 사운드 칸입니다. 이것도 편의용으로 제공해놨어요.
    
    Pair Door System - Paired Door 는 제가
    
    퀴즈를 풀면 양방향의 문이 동시에 Lock 에서 Open 으로 넘어가도록 코드를 구성해놓았습니다.
    
    Inspector = Paired Door는 반드시 양쪽 문에 서로를 연결해야 하고요
    
    퀴즈를 풀면 그 문 두개가 동시에 Lock 이 풀리도록 하기 위해서 넣어놨습니다.
    
    훗날 퀴즈와 관련된 스크립트를 작성하시는 분이 있다면
    
    public LockedDoor Door; 로 잠긴문 변수 선언 해주신다음
    
    Inspector 에서 잠긴문 오브젝트를 드래그 해주시고
    
    Door.SolvePuzzle(); 로 호출해주시면 문이 열릴겁니다.

---
+++ GhostAdventureProject/Assets/01.Scripts/Door/BaseDoor.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public abstract class BaseDoor : MonoBehaviour
+{
+    [Header("Door Settings")]
+   protected bool isLocked = false;
+    [SerializeField] protected Transform targetDoor; // 이동할 문 오브젝트 (드래그 앤 드롭)
+    [SerializeField] protected Vector2 targetPos; // 좌표 직접 입력 방식 (백업용)
+
+    protected bool playerNearby = false;
+
+    public bool IsLocked => isLocked;
+
+    protected virtual void Start()
+    {
+        // 기본 초기화
+    }
+
+    protected virtual void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            TryInteract();
+        }
+    }
+
+    protected abstract void TryInteract();
+
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+            Debug.Log("문 근처에 도착했습니다. E키를 눌러 상호작용하세요.");
+        }
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+        }
+    }
+
+    protected void TeleportPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            Vector3 teleportPosition;
+
+            // 우선 targetDoor 오브젝트 참조 방식 사용
+            if (targetDoor != null)
+            {
+                teleportPosition = targetDoor.position;
+                Debug.Log($"플레이어가 {targetDoor.name}로 텔레포트했습니다!");
+            }
+            // targetDoor가 없으면 좌표 직접 입력 방식 사용 (백업)
+            else
+            {
+                teleportPosition = targetPos;
+                Debug.Log($"플레이어가 {targetPos}로 텔레포트했습니다!");
+            }
+
+            player.transform.position = teleportPosition;
+        }
+    }
+}
---
+++ GhostAdventureProject/Assets/01.Scripts/Door/LockedDoor.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+
+public class LockedDoor : BaseDoor
+{
+    [Header("Locked Door Settings")]
+    [SerializeField] private string puzzleId = ""; // 퍼즐 식별자 (예: "LivingRoom_Kitchen")
+    [SerializeField] private AudioClip lockedSound;
+    [SerializeField] private AudioClip unlockSound;
+
+    [Header("Pair Door System")]
+    [SerializeField] private LockedDoor pairedDoor; // 페어 문 (양방향 연결)
+
+    private AudioSource audioSource;
+
+    protected override void Start()
+    {
+        isLocked = true; // 기본적으로 잠김
+        audioSource = GetComponent<AudioSource>();
+
+        // 페어 문 자동 연결 (둘 다 서로를 참조하도록)
+        if (pairedDoor != null && pairedDoor.pairedDoor != this)
+        {
+            pairedDoor.pairedDoor = this;
+        }
+    }
+
+    protected override void TryInteract()
+    {
+        if (isLocked)
+        {
+            Debug.Log("문이 잠겨있습니다!");
+            PlaySound(lockedSound);
+        }
+        else
+        {
+            // 문이 열려있다면 텔레포트
+            TeleportPlayer();
+        }
+    }
+
+    // 퍼즐 해결 시 호출하는 메서드
+    public void SolvePuzzle()
+    {
+        if (isLocked)
+        {
+            UnlockPair();
+            Debug.Log($"퍼즐 해결! {puzzleId} 페어 문들이 열렸습니다!");
+        }
+    }
+
+    // 페어 문 함께 열기
+    public void UnlockPair()
+    {
+        // 자신 열기
+        if (isLocked)
+        {
+            isLocked = false;
+            PlaySound(unlockSound);
+            Debug.Log($"{gameObject.name} 문이 열렸습니다!");
+            OnDoorUnlocked();
+        }
+
+        // 페어 문도 열기 (무한 루프 방지)
+        if (pairedDoor != null && pairedDoor.isLocked)
+        {
+            pairedDoor.isLocked = false;
+            pairedDoor.PlaySound(pairedDoor.unlockSound);
+            Debug.Log($"{pairedDoor.gameObject.name} 페어 문이 열렸습니다!");
+            pairedDoor.OnDoorUnlocked();
+        }
+    }
+
+    // 페어 문 함께 잠그기
+    public void LockPair()
+    {
+        // 자신 잠그기
+        if (!isLocked)
+        {
+            isLocked = true;
+            Debug.Log($"{gameObject.name} 문이 잠겼습니다!");
+        }
+
+        // 페어 문도 잠그기
+        if (pairedDoor != null && !pairedDoor.isLocked)
+        {
+            pairedDoor.isLocked = true;
+            Debug.Log($"{pairedDoor.gameObject.name} 페어 문이 잠겼습니다!");
+        }
+    }
+
+    private void OnDoorUnlocked()
+    {
+        // 문이 열렸을 때의 추가 효과
+        // 나중에 파티클, 애니메이션 등 추가 가능
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (audioSource != null && clip != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+
+    // 테스트용 메서드들
+    [ContextMenu("Test - Solve Puzzle")]
+    public void TestSolvePuzzle()
+    {
+        SolvePuzzle();
+    }
+
+    [ContextMenu("Test - Lock Pair")]
+    public void TestLockPair()
+    {
+        LockPair();
+    }
+
+    // 퍼즐 매니저에서 사용할 수 있는 프로퍼티
+    public string PuzzleId => puzzleId;
+}
