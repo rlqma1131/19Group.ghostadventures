17d4398cff786fc2f37ab04df13c0123bdc12351
hbks0224 <hbks1234@gmail.com>
2025-07-04 18:13:20 +0900

    feat: follow 카메라 임시

---
+++ GhostAdventureProject/Assets/01.Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public CinemachineVirtualCamera Camera; // Follow할 대상 
+    public GameObject Target; // Follow할 대상 오브젝트
+   // public CinemachineVirtualCamera  vcam;
+    void Start()
+    {
+        Target = GameObject.Find("ZoomCamera"); // Follow할 대상 오브젝트를 찾음
+
+        
+
+        Camera = Target.GetComponent<CinemachineVirtualCamera>(); // 현재 오브젝트의 Follow를 타겟으로 설정
+
+        Camera.Follow = this.transform; // 현재 오브젝트를 Follow 대상으로 설정
+
+
+    }
+}
