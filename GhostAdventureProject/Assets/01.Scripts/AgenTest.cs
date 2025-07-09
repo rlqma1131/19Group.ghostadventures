ab8db669e80e51606bac30b9a6e2fb508f74c1d7
hbks0224 <hbks1234@gmail.com>
2025-07-09 18:18:42 +0900

    Revert "test : 충돌 일어남"
    This reverts commit 0dd6ebc5733c4b1d32fff4dec92a4ba8da0a1b89.

---
+++ GhostAdventureProject/Assets/01.Scripts/AgenTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentTest : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent _agent;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        // 2D에서는 이부분이 필수
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        _agent.SetDestination(target.position);
+    }
+}
