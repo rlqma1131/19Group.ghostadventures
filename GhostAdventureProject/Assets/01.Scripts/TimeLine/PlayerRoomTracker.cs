aad6fd7df59322d48e65eb1c9296bbcc224fd6b6
hbks0224 <hbks1234@gmail.com>
2025-07-10 14:37:48 +0900

    feat : 맵 진입마다 맵 카운터 증가하는 PlayerRoomTracker 작성 및 각 방의 콜라이더마다 RoomInfo적용

---
+++ GhostAdventureProject/Assets/01.Scripts/TimeLine/PlayerRoomTracker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerRoomTracker : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if(other.tag == "Room")
+        {
+
+        RoomInfo room = other.GetComponent<RoomInfo>();
+        if (room != null)
+        {
+            room.roomCount++;
+            Debug.Log($"Entered {room.roomName}, count: {room.roomCount}");
+        }
+
+        }
+
+    }
+}
+ 
