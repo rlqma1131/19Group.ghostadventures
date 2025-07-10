6252b74a55e47479307158b7285386d967c9c178
song-ssi <song33@outlook.kr>
2025-07-10 23:18:52 +0900

    Feat: 아이템, 사용시 슬롯에서 삭제 구현

--- GhostAdventureProject/Assets/01.Scripts/UI/Inventory_PossessableObject.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/Inventory_PossessableObject.cs
@@ -169,7 +169,10 @@ public class Inventory_PossessableObject : Singleton<Inventory_PossessableObject
 
     public void UseItem(ItemData item, int amount)
     {
-        InventorySlot_PossessableObject slot = HaveItem.Instance.inventorySlots.Find(s => s.item == item);
+        InventorySlot_PossessableObject slot = spawnedSlots
+        .ConvertAll(s => s.GetComponent<InventorySlot_PossessableObject>())
+        .Find(s => s.item == item);        
+        
         if (slot != null)
         {
             slot.UseItem(amount);
