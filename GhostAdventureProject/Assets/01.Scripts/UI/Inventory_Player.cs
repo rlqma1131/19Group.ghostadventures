f022252ddd9e16f78415ffb2741988047ff66453
rlqma1131 <rlqma1131@gmail.com>
2025-07-10 09:55:45 +0900

    feat: 인벤토리 주석처리

--- GhostAdventureProject/Assets/01.Scripts/UI/Inventory_Player.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/Inventory_Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
@@ -106,27 +106,27 @@ public class Inventory_Player : MonoBehaviour
     //     }
     // }
 
-    private void Update()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            KeyCode key = keybindConfig.GetKeyForSlot(i);
+    //private void Update()
+    //{
+    //    for (int i = 0; i < 5; i++)
+    //    {
+    //        KeyCode key = keybindConfig.GetKeyForSlot(i);
 
-            if (Input.GetKeyDown(key))
-            {
-                int clueIndex = currentPage * cluesPerPage + i;
+    //        if (Input.GetKeyDown(key))
+    //        {
+    //            int clueIndex = currentPage * cluesPerPage + i;
 
-                if (InventoryExpandViewer.Instance.IsShowing())
-                {
-                    InventoryExpandViewer.Instance.HideClue();
-                }
-                else if (clueIndex < collectedClues.Count)
-                {
-                    InventoryExpandViewer.Instance.ShowClue(collectedClues[clueIndex]);
-                }
-            }
-        }
-    }
+    //            if (InventoryExpandViewer.Instance.IsShowing())
+    //            {
+    //                InventoryExpandViewer.Instance.HideClue();
+    //            }
+    //            else if (clueIndex < collectedClues.Count)
+    //            {
+    //                InventoryExpandViewer.Instance.ShowClue(collectedClues[clueIndex]);
+    //            }
+    //        }
+    //    }
+    //}
 }
 
 
