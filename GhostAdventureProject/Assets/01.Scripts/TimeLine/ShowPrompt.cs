a4573cb31fe3ea77404f4fc6a2ea93d758a18453
hbks0224 <hbks1234@gmail.com>
2025-07-11 10:01:19 +0900

    feat : 임시 곰인형 연출

---
+++ GhostAdventureProject/Assets/01.Scripts/TimeLine/ShowPrompt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPrompt : MonoBehaviour
+{
+
+
+
+    public void ShowPrompt_TimeLine(string dialog)
+    {
+
+
+        UIManager.Instance.PromptUI.ShowPrompt(dialog, 2f);
+
+    }
+
+}
---
+++ GhostAdventureProject/Assets/01.Scripts/TimeLine/SpriteAlphaControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpriteAlphaControl : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer[] Sprites;
+
+    [SerializeField] private float Alpha;
+    //[SerializeField] private float Duration;
+
+    private void Awake()
+    {
+        Sprites = transform.GetComponentsInChildren<SpriteRenderer>();
+
+
+    }
+
+    public void SetAlpha(float alpha)
+    {
+        foreach (var spriteRenderer in Sprites)
+        {
+            spriteRenderer.DOFade(alpha, 4);
+                
+        }
+    }
+}
