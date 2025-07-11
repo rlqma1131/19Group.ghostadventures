973a845760e96366563f5d746e843e6262b5b059
song-ssi <song33@outlook.kr>
2025-07-11 14:47:42 +0900

    Feat: 기억노드 클릭시 씬 이동 구현

--- GhostAdventureProject/Assets/01.Scripts/UI/MemoryNode.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/MemoryNode.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 
 public class MemoryNode : MonoBehaviour
 {
     // 컷씬 이미지를 가져와야 하는데 어디서 가져와야함?
 [SerializeField] private Image icon;
 [SerializeField] private TextMeshProUGUI name;
+private string sceneName;
 
     private MemoryData memory;
 
     public void Initialize(MemoryData memoryData)
     {
         memory = memoryData;
-        icon.sprite = memory.MemoryObjectSprite;
+        icon.sprite = memory.MemoryCutSceneImage;
         name.text = memory.memoryTitle;
+        sceneName = memory.memoryConnectSceneName;
 
     //     switch (memory.type)
     //     {
@@ -32,5 +36,12 @@ public class MemoryNode : MonoBehaviour
     //             break;
     //     }
     }
+
+    public void GoToCutScene()
+    {
+        Debug.Log("씬 다시보기 버튼클릭");
+        SceneManager.LoadScene(sceneName);
+    }
+    
 
 }
