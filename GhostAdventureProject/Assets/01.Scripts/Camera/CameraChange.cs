404d3eca79ffa377502f5591da6dc448d94924c5
hbks0224 <hbks1234@gmail.com>
2025-07-14 22:08:18 +0900

    feat : 플레이어프리팹 하위에 맵 감지 콜라이더 생성 (맵이름 중복호출을 막기 위함)

--- GhostAdventureProject/Assets/01.Scripts/Camera/CameraChange.cs
+++ GhostAdventureProject/Assets/01.Scripts/Camera/CameraChange.cs
@@ -1,6 +1,7 @@
 ﻿using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraChange : MonoBehaviour
@@ -11,6 +12,8 @@ public class CameraChange : MonoBehaviour
     private int CurrentPriority = 10; // 현재 카메라 우선순위
     private int ActivePriority = 13; // 활성화된 카메라 우선순위
 
+    [SerializeField] private UITweenAnimator uITweenAnimator; // UI 애니메이션 컴포넌트
+    [SerializeField] private TextMeshProUGUI text; // 방 이름 텍스트 컴포넌트
 
 
 
@@ -21,6 +24,7 @@ public class CameraChange : MonoBehaviour
             // 플레이어가 카메라 변경 영역에 들어오면 현재 카메라의 우선순위를 낮추고
             // 새로운 카메라의 우선순위를 높입니다.
             Vcam.Priority = ActivePriority;
+
         }
     }
 
