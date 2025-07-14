065083946c0e5b9d15f68fa2d280771fe91aa1d8
hbks0224 <hbks1234@gmail.com>
2025-07-14 15:22:40 +0900

    feat : 맵 이동시 맵 이름 출력

--- GhostAdventureProject/Assets/01.Scripts/TimeLine/PlayerRoomTracker.cs
+++ GhostAdventureProject/Assets/01.Scripts/TimeLine/PlayerRoomTracker.cs
@@ -1,7 +1,17 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class PlayerRoomTracker : MonoBehaviour
 {
+    [SerializeField] private UITweenAnimator uITweenAnimator; // UI 애니메이션 컴포넌트
+    [SerializeField] private TextMeshProUGUI text; // 프롬프트 컴포넌트
+
+    private void Awake()
+    {
+        uITweenAnimator = FindObjectOfType<UITweenAnimator>();
+        text = GameObject.Find("InfoText").GetComponent<TextMeshProUGUI>();
+
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
 
@@ -13,9 +23,16 @@ public class PlayerRoomTracker : MonoBehaviour
         {
             room.roomCount++;
             Debug.Log($"Entered {room.roomName}, count: {room.roomCount}");
+
+
+                //맵이름 두두둥장
+                text.text = $"{room.roomName}";
+                uITweenAnimator.SlideInAndOut();
         }
 
         }
+
+       
 
     }
 }
---
+++ GhostAdventureProject/Assets/01.Scripts/UI/UITweenAnimator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class UITweenAnimator : MonoBehaviour
+{
+    public RectTransform uiElement;
+    public float duration = 0.5f;
+    public float startX = -788f;
+    public float endX = 0f;
+
+    
+    public float waitTime = 1.0f; // UI가 화면에 머무는 시간 (1초)
+
+    public void SlideInAndOut()
+    {
+        //초기 위치 설정
+
+        Vector2 startPos = new Vector2(startX, uiElement.anchoredPosition.y);
+        uiElement.anchoredPosition = startPos;
+
+        
+        Sequence sequence = DOTween.Sequence();
+
+        //슬라이드 인 애니메이션
+        sequence.Append(
+            uiElement.DOAnchorPosX(endX, duration)
+                     .SetEase(Ease.OutQuad)
+        );
+
+        //  대기 시간
+        sequence.AppendInterval(waitTime);
+
+        // 슬라이드 아웃 애니메이션
+        sequence.Append(
+            uiElement.DOAnchorPosX(startX, duration)
+                     .SetEase(Ease.InQuad) 
+        );
+
+        
+        sequence.Play();
+    }
+
+
+}
