90facea171ea10466320965f887b18116dd0afac
DounPark <ehdns1562@gmail.com>
2025-07-15 19:37:34 +0900

    refactor: 문 전체 연결 및 리팩토링

--- GhostAdventureProject/Assets/01.Scripts/Door/BaseDoor.cs
+++ GhostAdventureProject/Assets/01.Scripts/Door/BaseDoor.cs
@@ -12,8 +12,9 @@ public abstract class BaseDoor : BaseInteractable
     [SerializeField] protected GameObject OpenObject;      // WoodDoor_Side (열린 상태)
 
     protected bool playerNearby = false;
-    public bool IsLocked => isLocked;
     protected SpriteRenderer spriteRenderer;
+    private bool previousLockedState = false;
+    public bool IsLocked => isLocked;
 
     protected virtual void Start()
     {
@@ -28,12 +29,6 @@ public abstract class BaseDoor : BaseInteractable
             TryInteract();
         }
 
-        // TODO: 테스트 완료 후 삭제 필요 - V키 테스트 코드
-        if (playerNearby && Input.GetKeyDown(KeyCode.V))
-        {
-            TestToggleDoor();
-        }
-
         SetInteractionPopup(playerNearby);
     }
 
@@ -41,73 +36,39 @@ public abstract class BaseDoor : BaseInteractable
 
     protected override void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
+        if (other.CompareTag("Player")) 
             playerNearby = true;
-            Debug.Log("문 근처에 도착했습니다. E키를 눌러 상호작용하세요.");
-        }
+        
     }
 
     protected override void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
-        {
             playerNearby = false;
-        }
     }
 
     protected void TeleportPlayer()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null)
-        {
-            Vector3 teleportPosition;
+        GameObject player = GameManager.Instance?.Player;
+        if (player == null)
+            return;
 
-            // 우선 targetDoor 오브젝트 참조 방식 사용
-            if (targetDoor != null)
-            {
-                teleportPosition = targetDoor.position;
-                Debug.Log($"플레이어가 {targetDoor.name}로 텔레포트했습니다!");
-            }
-            // targetDoor가 없으면 좌표 직접 입력 방식 사용 (백업)
-            else
-            {
-                teleportPosition = targetPos;
-                Debug.Log($"플레이어가 {targetPos}로 텔레포트했습니다!");
-            }
+        Vector3 teleportPosition = targetDoor != null ? targetDoor.position : (Vector3)targetPos;
 
-            player.transform.position = teleportPosition;
-        }
+        player.transform.position = teleportPosition;
     }
 
     protected void UpdateDoorVisual()
     {
-        if (isLocked)
-        {
-            // 잠긴 상태: Close 스프라이트 보이기, Side 스프라이트 숨기기
-            if (closedObject != null)
-                closedObject.SetActive(true);
-            if (OpenObject != null)
-                OpenObject.SetActive(false);
-        }
-        else
-        {
-            // 열린 상태: Close 스프라이트 숨기기, Side 스프라이트 보이기
-            if (closedObject != null)
-                closedObject.SetActive(false);
-            if (OpenObject != null)
-                OpenObject.SetActive(true);
-        }
-    }
-
-    // TODO: 테스트 완료 후 삭제 필요 - 테스트용 메서드
-    protected virtual void TestToggleDoor()
-    {
-        isLocked = !isLocked;
-        UpdateDoorVisual();
-
-        string status = isLocked ? "잠김" : "열림";
-        Debug.Log($"[테스트] 문 상태 변경: {status}");
+        if(isLocked == previousLockedState)
+            return; // 상태변경없으면 무시
+        
+        previousLockedState = isLocked;
+        
+        if(closedObject != null)
+            closedObject.SetActive(isLocked);
+        if(OpenObject != null)
+            OpenObject.SetActive(!isLocked);
     }
 
     // Enemy가 접근할 수 있는 public 메서드들
