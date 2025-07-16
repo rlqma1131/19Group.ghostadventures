73c3e6ba7bca23bda0714fbc981dcb324ddc63fc
song-ssi <song33@outlook.kr>
2025-07-16 10:20:46 +0900

    Feat: 빙의 인벤토리 수정중

--- GhostAdventureProject/Assets/01.Scripts/UI/HaveItem.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/HaveItem.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-//using UnityEditor.Search;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class HaveItem : Singleton<HaveItem>
