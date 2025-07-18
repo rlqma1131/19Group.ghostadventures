83978ce03dd97e0dd870dff8fabe30fc57885314
DounPark <ehdns1562@gmail.com>
2025-07-18 20:15:13 +0900

    feat: 관리실 지하수로 퍼즐, 미로 생성

---
+++ GhostAdventureProject/Assets/01.Scripts/Managers/Puzzle/Ch2_SewerPuzzleManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch2_SewerPuzzleManager : MonoBehaviour
+{
+    public static Ch2_SewerPuzzleManager Instance;
+    [SerializeField] private GameObject mazeGroup;
+
+    private bool isSolved = false;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        mazeGroup.SetActive(!isSolved);
+    }
+
+    public void SetPuzzleSolved()
+    {
+        isSolved = true;
+        mazeGroup.SetActive(false);
+        Debug.Log(" 지하수로 퍼즐 완료 ");
+    }
+
+    public bool IsPuzzleSolved() => isSolved;
+}
