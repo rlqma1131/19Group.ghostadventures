76025190635f505827eb2a79e57606c1b2343ea0
DounPark <ehdns1562@gmail.com>
2025-07-19 00:53:12 +0900

    refactor: 트리거 부분 플레이어 start에서 미리 캐싱후 사용

--- GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Obect/Ch2_SewerLightingTrigger.cs
+++ GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Obect/Ch2_SewerLightingTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,18 +11,24 @@ public class Ch2_SewerLightingTrigger : MonoBehaviour
     private Color originalGlobalColor;
     private bool hasStoredOriginal = false;
     private Light2D playerLight;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameManager.Instance.Player;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject == GameManager.Instance.Player)
+        if (other.gameObject == player)
         {
-            // 1. 플레이어 Light2D 자동 탐색
+            // Light2D 자동 탐색
             if (playerLight == null)
             {
                 playerLight = other.GetComponentInChildren<Light2D>(includeInactive: true);
             }
 
-            // 2. Global Light 어둡게
+            // Global Light 어둡게
             if (globalLight != null)
             {
                 if (!hasStoredOriginal)
@@ -34,7 +41,7 @@ public class Ch2_SewerLightingTrigger : MonoBehaviour
                 globalLight.intensity = 1f;
             }
 
-            // 3. Player Light 켜기
+            // Player Light 켜기
             if (playerLight != null)
             {
                 playerLight.enabled = true;
@@ -44,18 +51,16 @@ public class Ch2_SewerLightingTrigger : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject == GameManager.Instance.Player && !other.gameObject.activeInHierarchy)
+        if (other.gameObject == player && !other.gameObject.activeInHierarchy)
             return;
         
-        if (other.gameObject == GameManager.Instance.Player)
+        if (other.gameObject == player)
         {
-            // Global Light 복원
             if (globalLight != null && hasStoredOriginal)
             {
                 globalLight.color = originalGlobalColor;
             }
-
-            // Player Light 끄기
+            
             if (playerLight != null)
             {
                 playerLight.enabled = false;
@@ -72,7 +77,7 @@ public class Ch2_SewerLightingTrigger : MonoBehaviour
 
         if (playerLight == null)
         {
-            playerLight = GameManager.Instance.Player?.GetComponentInChildren<Light2D>(includeInactive: true);
+            playerLight = player?.GetComponentInChildren<Light2D>(includeInactive: true);
         }
 
         if (playerLight != null)
