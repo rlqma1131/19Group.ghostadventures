92795051b69e0d97a32f33c7b4f19be5b171f549
hbks0224 <hbks1234@gmail.com>
2025-07-18 23:55:31 +0900

    feat : 사신이 플레이어 근처 오면 화면 붉게

---
+++ GhostAdventureProject/Assets/01.Scripts/EnemyChaser/EnemyVolumeTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class EnemyVolumeTrigger : MonoBehaviour
+{
+   [SerializeField] private Volume globalVolume;
+    void Start()
+    {
+        globalVolume = GetComponentInChildren<Volume>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (globalVolume != null)
+            {
+                globalVolume.enabled = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (globalVolume != null)
+            {
+                globalVolume.enabled = false;
+            }
+        }
+    }
+
+
+}
