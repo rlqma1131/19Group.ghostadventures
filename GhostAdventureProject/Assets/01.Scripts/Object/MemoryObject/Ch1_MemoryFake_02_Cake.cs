ae5be1fb2e4520ed5292f653f890a375d30d0e4a
hbks0224 <hbks1234@gmail.com>
2025-07-21 10:20:27 +0900

    feat : 챕터1 케이크 기억 수집 후 케이크 빛나게 수정

--- GhostAdventureProject/Assets/01.Scripts/Object/MemoryObject/Ch1_MemoryFake_02_Cake.cs
+++ GhostAdventureProject/Assets/01.Scripts/Object/MemoryObject/Ch1_MemoryFake_02_Cake.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Ch1_MemoryFake_02_Cake : MemoryFragment
 {
     private Animator anim;
-
+    private ParticleSystem particleSystem;
+    private Light2D light;
     void Start()
     {
         isScannable = false;
         anim = GetComponentInChildren<Animator>();
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+        light = GetComponentInChildren<Light2D>();
     }
 
     public void ActivateCake()
@@ -18,7 +23,15 @@ public class Ch1_MemoryFake_02_Cake : MemoryFragment
     public override void AfterScan()
     {
         anim.SetTrigger("Show");
+        Invoke(nameof(AfterScanEffect), 8f); // 애니메이션 재생 후 효과 실행
         isScannable = false;
         ChapterEndingManager.Instance.CollectCh1Clue("H");
+    }
+    private void AfterScanEffect()
+    {
+
+    
+        particleSystem.Play();
+        light.enabled = true;
     }
 }
