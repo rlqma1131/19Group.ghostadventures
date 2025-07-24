556eee0a37e83624b7724250aa01eb08e2be63f3
rlqma1131 <rlqma1131@gmail.com>
2025-07-24 20:30:26 +0900

    feat: 배전반 에셋 수정 / 챕터3 Xray 퍼즐 진행 중

---
+++ GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Obect/Ch3_Xray.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch3_Xray : BasePossessable
+{
+    [Header("X-Ray 화면")]
+    [SerializeField] private Ch3_Xray_Monitor monitor;
+
+    [Header("사진")]
+    [SerializeField] private Sprite photo01;
+    [SerializeField] private Sprite photo02;
+    [SerializeField] private Sprite photo03;
+    [SerializeField] private Sprite photo04;
+
+    [Header("조작키")]
+    [SerializeField] private GameObject aKey;
+    [SerializeField] private GameObject dKey;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Unpossess();
+            aKey.SetActive(false);
+            dKey.SetActive(false);
+        }
+
+        // 좌우 움직임에 따라 photo 화면도 움직이기
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+        }
+    }
+
+    public override void OnPossessionEnterComplete()
+    {
+        aKey.SetActive(true);
+        dKey.SetActive(true);
+    }
+}
