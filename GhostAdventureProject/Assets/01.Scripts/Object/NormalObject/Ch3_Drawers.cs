2d35e909f9d2299c3fd90a2736d32c8a155dfe2c
rlqma1131 <rlqma1131@gmail.com>
2025-07-25 18:02:10 +0900

    feat: 챕터3 Xray 완료 / 환자서류 진행 중

---
+++ GhostAdventureProject/Assets/01.Scripts/Object/NormalObject/Ch3_Drawers.cs
@@ -0,0 +1,37 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class Ch3_Drawers : BasePossessable
+{
+    [Header("줌 카메라")]
+    [SerializeField] private CinemachineVirtualCamera zoomCamera;
+
+    protected override void Start()
+    {
+        isPossessed = false;
+        hasActivated = false;
+        zoomCamera.Priority = 5;
+    }
+
+    protected override void Update()
+    {
+        if (!isPossessed)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            zoomCamera.Priority = 5;
+
+            UIManager.Instance.PlayModeUI_OpenAll();
+
+            Unpossess();
+        }
+    }
+
+    public override void OnPossessionEnterComplete()
+    {
+        zoomCamera.Priority = 20;
+
+        UIManager.Instance.PlayModeUI_CloseAll();
+    }
+}
--- GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Obect/Ch3_Xray.cs
+++ GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Obect/Ch3_Xray.cs
@@ -1,25 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Ch3_Xray : BasePossessable
 {
-    [Header("X-Ray 화면")]
-    [SerializeField] private Ch3_Xray_Monitor monitor;
+    [Header("X-Ray 기계")]
+    [SerializeField] private GameObject[] xRayHead; // 머리 ~ 발 순서
+
+    [Header("X-Ray 줌 화면")]
+    [SerializeField] Image zoomPhotoScreen;
 
     [Header("사진")]
-    [SerializeField] private Sprite photo01;
-    [SerializeField] private Sprite photo02;
-    [SerializeField] private Sprite photo03;
-    [SerializeField] private Sprite photo04;
-
+    [SerializeField] private Image[] zoomPhotos; // 머리 ~ 발 사진들
+    
     [Header("조작키")]
     [SerializeField] private GameObject aKey;
     [SerializeField] private GameObject dKey;
 
+    private int currentPhotoIndex = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        aKey.SetActive(false);
+        dKey.SetActive(false);
+
+        currentPhotoIndex = 0;
+        UpdateXrayDisplay();
+    }
+
     protected override void Update()
     {
         base.Update();
+
+        if (!isPossessed) return;
 
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -27,13 +41,32 @@ public class Ch3_Xray : BasePossessable
             aKey.SetActive(false);
             dKey.SetActive(false);
         }
-
-        // 좌우 움직임에 따라 photo 화면도 움직이기
         else if (Input.GetKeyDown(KeyCode.D))
         {
+            if (currentPhotoIndex < zoomPhotos.Length - 1)
+            {
+                currentPhotoIndex++;
+                UpdateXrayDisplay();
+            }
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
+            if (currentPhotoIndex > 0)
+            {
+                currentPhotoIndex--;
+                UpdateXrayDisplay();
+            }
+        }
+    }
+    private void UpdateXrayDisplay()
+    {
+        // 줌 이미지 전환
+        zoomPhotoScreen.sprite = zoomPhotos[currentPhotoIndex].sprite;
+
+        // xRayHead 위치 이동
+        for (int i = 0; i < xRayHead.Length; i++)
+        {
+            xRayHead[i].SetActive(i == currentPhotoIndex);
         }
     }
 
