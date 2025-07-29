7b42ca8776477725a418bbea20e24418bd8a7b88
rlqma1131 <rlqma1131@gmail.com>
2025-07-29 14:24:32 +0900

    feat: 카세트 플레이어 퍼즐 진행 중

---
+++ GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Obect/Ch3_CassettePlayer.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch3_CassettePlayer : BasePossessable
+{
+    [Header("소리 목록")]
+    [SerializeField] private AudioClip talkingSound; // 대화 소리
+    [SerializeField] private AudioClip glitchSound; // 퍼즐 해결 후 소리
+
+    private bool isPlaying = false; // 재생 여부
+    private bool isSolved = false; // 문제 해결 여부
+
+    private float answerValue; // 정답 주파수 조정값
+    private float answerSpeed; // 정답 재생 속도
+
+    protected override void Update()
+    {
+        if (!isPossessed)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Unpossess();
+            //SoundManager.Instance.ChangeBGM(챕터3BGM);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (isSolved)
+            {
+                // 퍼즐 해결 후
+                SoundManager.Instance.StopBGM();
+                SoundManager.Instance.ChangeBGM(glitchSound);
+            }
+            else
+            {
+                // 퍼즐 해결 전
+                PlayTalkingSound();
+            }
+        }
+
+        if (!isPlaying)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //cutoffFrequency 증가
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            //cutoffFrequency 감소
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            // 재생 속도 증가
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            // 재생 속도 감소
+        }
+
+        CheckSolved();
+    }
+
+    void PlayTalkingSound() // 토글
+    {
+        isPlaying = true;
+        SoundManager.Instance.ChangeBGM(talkingSound);
+
+        // 혹은 SoundManager.Instance.StopBGM();
+        // isPlaying = false;
+    }
+
+    void CheckSolved()
+    {
+        if(cutoffFrequency == answerValue && 재생 속도 == answerSpeed)
+        { 
+            hasActivated = false; // 빙의 불가
+
+            isPlaying = false; // 재생 중지
+            isSolved = true;
+            SoundManager.Instance.StopBGM();
+            SoundManager.Instance.PlaySFX(talkingSound);
+
+            // 재생 끝나면
+            SoundManager.Instance.ChangeBGM(glitchSound);
+            // 종이 
+        }
+    }
+}
+
--- GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Person/Ch3_Nurse.cs
+++ GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Person/Ch3_Nurse.cs
@@ -1,18 +1,171 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ch3_Nurse : MonoBehaviour
+enum NurseState
 {
-    // Start is called before the first frame update
-    void Start()
+    Work,
+    Rest
+}
+
+public class Ch3_Nurse : MoveBasePossessable
+{
+    [Header("위치")]
+    [SerializeField] private Transform[] workPositions; // 일 하는 두 지점
+    [SerializeField] private Transform restPosition;    // 쉴 위치
+
+    [Header("시간")]
+    [SerializeField] private float waitDuration = 3f;   // 각 지점 대기 시간
+    [SerializeField] private float stateChangeInterval = 15f; // 상태 변경 주기
+
+    private Person person;
+    private PersonConditionHandler conditionHandler;
+    private NurseState state = NurseState.Work;
+    private int currentWorkIndex = 0;
+    private float waitTimer = 0f;
+    private float stateTimer = 0f;
+    private bool isWaiting = false;
+
+    protected override void Start()
     {
-        
+        base.Start();
+        moveSpeed = 2f;
+        person.currentCondition = PersonCondition.Tired;
+        conditionHandler = new VitalConditionHandler();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        // 빙의 상태
+        if (isPossessed)
+        {
+            if (!PossessionSystem.Instance.CanMove)
+                return;
+             
+            Move();
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                zoomCamera.Priority = 5;
+                Unpossess();
+            }
+        }
+
+        if (isPossessed) return;
+
+        // 빙의 상태가 아닐 때
+        stateTimer += Time.deltaTime;
+        if (stateTimer >= stateChangeInterval)
+        {
+            ToggleState();
+            stateTimer = 0f;
+        }
+
+        // 일정 주기마다 상태 변경
+        switch (state)
+        {
+            case NurseState.Work:
+                person.currentCondition = PersonCondition.Tired;
+                HandleWork();
+                break;
+            case NurseState.Rest:
+                person.currentCondition = PersonCondition.Vital;
+                HandleRest();
+                break;
+        }
+
+        // 컨디션 UI & QTE 업데이트
+        SetCondition(person.currentCondition);
+    }
+
+    private void HandleWork()
+    {
+        if (isWaiting)
+        {
+            waitTimer += Time.deltaTime;
+            if (waitTimer >= waitDuration)
+            {
+                // 다음 지점으로 이동
+                isWaiting = false;
+                currentWorkIndex = (currentWorkIndex + 1) % workPositions.Length;
+            }
+            else
+            {
+                // 일하기
+                SetMoveAnimation(false);
+                anim.Play("Work");
+                return;
+            }
+        }
+
+        Transform target = workPositions[currentWorkIndex];
+        MoveTo(target.position);
+    }
+
+    private void HandleRest()
+    {
+        if (Vector2.Distance(transform.position, restPosition.position) > 0.1f)
+        {
+            MoveTo(restPosition.position);
+        }
+        else
+        {
+            SetMoveAnimation(false);
+        }
+    }
+
+    private void MoveTo(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        SetMoveAnimation(true);
+        if (spriteRenderer != null)
+            spriteRenderer.flipX = direction.x < 0;
+
+        if (Vector2.Distance(transform.position, target) < 0.1f)
+        {
+            isWaiting = true;
+            waitTimer = 0f;
+        }
+    }
+
+    private void SetMoveAnimation(bool isMoving)
+    {
+        if (anim != null)
+            anim.SetBool("Move", isMoving);
+    }
+
+    private void ToggleState()
+    {
+        if (state == NurseState.Work)
+        {
+            state = NurseState.Rest;
+        }
+        else
+        {
+            state = NurseState.Work;
+            currentWorkIndex = 0;
+            isWaiting = false;
+        }
+    }
+
+    public void SetCondition(PersonCondition condition)
+    {
+        person.currentCondition = condition;
+        switch (condition)
+        {
+            case PersonCondition.Vital:
+                conditionHandler = new VitalConditionHandler();
+                break;
+            case PersonCondition.Normal:
+                conditionHandler = new NormalConditionHandler();
+                break;
+            case PersonCondition.Tired:
+                conditionHandler = new TiredConditionHandler();
+                break;
+        }
+        QTESettings qteSettings = conditionHandler.GetQTESettings();
+        UIManager.Instance.QTE_UI_3.ApplySettings(qteSettings);
     }
 }
