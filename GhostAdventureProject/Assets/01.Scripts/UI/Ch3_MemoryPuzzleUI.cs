a8678f2ef262f745323d18ffdffcf6e7176d98e3
hbks0224 <hbks1234@gmail.com>
2025-08-04 20:29:54 +0900

    fix : 최종퍼즐 스크립트 수정

--- GhostAdventureProject/Assets/01.Scripts/UI/Ch3_MemoryPuzzleUI.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/Ch3_MemoryPuzzleUI.cs
@@ -196,6 +196,9 @@ public class Ch3_MemoryPuzzleUI : MonoBehaviour
 
         MemoryManager.Instance.TryCollect(memoryData); // 기억 조각 수집
 
+        clearDoor.OpenDoor();
+        gameObject.SetActive(false);
+        puzzlecompleted = true;
 
         SaveData data = new SaveData
         {
@@ -212,9 +215,6 @@ public class Ch3_MemoryPuzzleUI : MonoBehaviour
         };
 
         SaveManager.SaveGame(data);
-        clearDoor.OpenDoor();
-        gameObject.SetActive(false);
-        puzzlecompleted = true;
         yield return new WaitForSeconds(0.5f);
 
     }
