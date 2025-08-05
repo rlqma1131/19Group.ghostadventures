28705c38662a44bbfb28f00e22faedaba7df7dd1
hbks0224 <hbks1234@gmail.com>
2025-08-05 12:26:01 +0900

    fix : 브금 없는 컷신 브금 추가

--- GhostAdventureProject/Assets/01.Scripts/UI/StartSceneUI.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/StartSceneUI.cs
@@ -19,6 +19,29 @@ public class StartSceneUI : MonoBehaviour
 
     }
 
+    public void Ch02Start()
+    {
+
+        Debug.Log("Ch02Start버튼 클릭");
+        SceneManager.LoadScene("Ch01_To_Ch02");
+        if (UIManager.Instance != null)
+        {
+            UIManager.Instance.PlayModeUI_CloseAll();
+            UIManager.Instance.startEndingUI_CloseAll();
+        }
+    }
+
+    public void Ch03Start()
+    {
+
+        Debug.Log("Ch03Start버튼 클릭");
+        SceneManager.LoadScene("Ch02_To_Ch03");
+        if (UIManager.Instance != null)
+        {
+            UIManager.Instance.PlayModeUI_CloseAll();
+            UIManager.Instance.startEndingUI_CloseAll();
+        }
+    }
     public void ExitGameButton()
     {
 #if UNITY_EDITOR
