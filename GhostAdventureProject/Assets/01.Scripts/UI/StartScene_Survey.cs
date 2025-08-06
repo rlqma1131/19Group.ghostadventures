e4f1295113449fcbb3f3ec5c43211a65cf0058e4
hbks0224 <hbks1234@gmail.com>
2025-08-06 18:22:01 +0900

    feat : 설문조사 버튼 애니메이션

--- GhostAdventureProject/Assets/01.Scripts/UI/StartScene_Survey.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/StartScene_Survey.cs
@@ -1,18 +1,66 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class StartScene_Surveyh : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Image targetImage;
+
+
+    private void Awake()
+    {
+        targetImage = GetComponent<Image>();
+    }
+    private void Start()
     {
         
+        
+        Vector3  targetScale = transform.localScale * 1.2f;
+
+        transform.DOScale(targetScale, 0.5f).SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutSine);
+
+
+
+
+        StartRainbowColorLoop();
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartRainbowColorLoop()
     {
-        
+        if (targetImage == null)
+        {
+            Debug.LogWarning("targetImage가 할당되지 않았습니다!");
+            return;
+        }
+
+        // 무지개 색상 배열 (빨, 주, 노, 초, 파, 남, 보)
+        Color[] rainbowColors = new Color[]
+        {
+            Color.red,
+            new Color(1f, 0.5f, 0f), // 주황
+            Color.yellow,
+            Color.green,
+            Color.cyan,
+            Color.blue,
+            new Color(0.5f, 0f, 1f) // 보라
+        };
+
+        float durationPerColor = 0.5f;
+
+        Sequence rainbowSequence = DOTween.Sequence();
+
+        foreach (Color color in rainbowColors)
+        {
+            rainbowSequence.Append(targetImage.DOColor(color, durationPerColor));
+        }
+
+        rainbowSequence.SetLoops(-1);
     }
+
 }
