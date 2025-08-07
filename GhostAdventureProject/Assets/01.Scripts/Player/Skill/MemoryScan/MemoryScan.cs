4285000f9499dd2af24232f9eba1094e70bb7d9f
song-ssi <song33@outlook.kr>
2025-08-07 11:43:15 +0900

    Fix: using VisualScripting 삭제

--- GhostAdventureProject/Assets/01.Scripts/Player/Skill/MemoryScan/MemoryScan.cs
+++ GhostAdventureProject/Assets/01.Scripts/Player/Skill/MemoryScan/MemoryScan.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using Unity.VisualScripting;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class MemoryScan : MonoBehaviour
--- GhostAdventureProject/Assets/01.Scripts/UI/ESCMenu.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/ESCMenu.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
