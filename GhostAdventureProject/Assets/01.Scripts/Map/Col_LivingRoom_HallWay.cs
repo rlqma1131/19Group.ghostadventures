2fb6e2bbd75c5c445697b1c4b94bdf129084c9b2
song-ssi <song33@outlook.kr>
2025-08-07 11:10:04 +0900

    Fix: 1챕 오류 수정
    - col_livingroom_hallway 콜라이더 수정
    - 깜짝상자 키 튜토리얼 콜라이더 삭제

--- GhostAdventureProject/Assets/01.Scripts/Map/Col_LivingRoom_HallWay.cs
+++ GhostAdventureProject/Assets/01.Scripts/Map/Col_LivingRoom_HallWay.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class Col_LivingRoom_HallWay : MonoBehaviour
 {
     BoxCollider2D[] cols;
+    BoxCollider2D maincol;
     [SerializeField] private GameObject icon;
+    private bool onTrigger;
 
     void Start()
     {
-        cols = GetComponentsInChildren<BoxCollider2D>();
-        icon.SetActive(false);
+        maincol = GetComponent<BoxCollider2D>();
+        // cols = GetComponentsInChildren<BoxCollider2D>();
+        // icon.SetActive(false);
     }
 
     void Update()
     {
-        if(PuzzleStateManager.Instance.IsPuzzleSolved("티비"))
+        if(PuzzleStateManager.Instance.IsPuzzleSolved("티비") && !onTrigger)
         {
-            foreach (var col in cols)
-            {
-                col.isTrigger = true;
-            }
+            maincol.isTrigger = true;
+            // foreach (var col in cols)
+            // {
+            //     col.isTrigger = true;
+                
+            // }
+            onTrigger = true;
         }
     }
     void OnTriggerEnter2D(Collider2D collision)
@@ -29,15 +35,18 @@ public class Col_LivingRoom_HallWay : MonoBehaviour
         {
             UIManager.Instance.PromptUI.ShowPrompt("일단 TV를 켜야 해");
         }
+        if(icon == null) return;
 
         if(PuzzleStateManager.Instance.IsPuzzleSolved("티비"))
         {
+            
             icon.SetActive(true);  // 트리거로 변경
         }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
+        if(icon == null) return;
         if(PuzzleStateManager.Instance.IsPuzzleSolved("티비"))
         {
             icon.SetActive(false);  // 트리거로 변경
