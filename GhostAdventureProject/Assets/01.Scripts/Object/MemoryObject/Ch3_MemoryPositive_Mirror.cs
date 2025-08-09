717be10b6d8e46c8ee9cedb007b09a830333ea95
rlqma1131 <rlqma1131@gmail.com>
2025-08-09 23:24:35 +0900

    feat: 최종퍼즐 활성화 / 카메라 셰이크 적용

--- GhostAdventureProject/Assets/01.Scripts/Object/MemoryObject/Ch3_MemoryPositive_Mirror.cs
+++ GhostAdventureProject/Assets/01.Scripts/Object/MemoryObject/Ch3_MemoryPositive_Mirror.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Ch3_MemoryPositive_Mirror : MemoryFragment
 {
-    [SerializeField] private Ch3_Scanner scanner;
+    private Ch3_Scanner scanner;
+
+    void Start()
+    {
+        scanner = FindObjectOfType<Ch3_Scanner>();
+    }
 
     public void ActivateObj()
     {
