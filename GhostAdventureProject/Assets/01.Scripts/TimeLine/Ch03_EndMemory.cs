62f1129775d2a2ff639a1e04abda6d386109be9b
rlqma1131 <rlqma1131@gmail.com>
2025-08-12 15:58:59 +0900

    feat: 자동진행 기억 저장되도록

--- GhostAdventureProject/Assets/01.Scripts/TimeLine/Ch03_EndMemory.cs
+++ GhostAdventureProject/Assets/01.Scripts/TimeLine/Ch03_EndMemory.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Ch03_EndMemory : MonoBehaviour
+public class Ch03_EndMemory : MemoryFragment
 {
     [SerializeField] private MemoryData memoryData;
     bool activeCutscene = false;
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected override void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !activeCutscene)
         {
@@ -23,13 +23,24 @@ public class Ch03_EndMemory : MonoBehaviour
     private IEnumerator LoadNextSceneAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
-
-
-
+        
         Inventory_Player _inventory = GameManager.Instance.Player.GetComponent<Inventory_Player>();
         MemoryManager.Instance.TryCollect(memoryData);
         SoundManager.Instance.FadeOutAndStopLoopingSFX();
         SceneManager.LoadScene("Ch03_End", LoadSceneMode.Additive);
-        ChapterEndingManager.Instance.RegisterScannedMemory(memoryData.memoryID, 3);
+
+        if (TryGetComponent(out UniqueId uid))
+            SaveManager.SetMemoryFragmentScannable(uid.Id, isScannable);
+
+        var chapter = DetectChapterFromScene(SceneManager.GetActiveScene().name);
+        ChapterEndingManager.Instance.RegisterScannedMemory(data.memoryID, chapter);
+
+        SaveManager.SaveWhenScanAfter(data.memoryID, data.memoryTitle,
+            SceneManager.GetActiveScene().name,
+            GameManager.Instance.Player.transform.position,
+            checkpointId: data.memoryID,
+            autosave: true);
+
+        Debug.Log($"[MemoryFragment] 진행도 저장됨 : {data.memoryID} / {data.memoryTitle}");
     }
 }
