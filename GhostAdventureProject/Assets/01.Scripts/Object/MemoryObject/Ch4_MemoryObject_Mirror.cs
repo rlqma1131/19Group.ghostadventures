18fbf1bda93f3b9fa4cb35663a25a6994e883bca
Bangeunseong <bangeunseong@naver.com>
2025-11-02 22:28:30 +0900

    [feat] ch4 기억 5 오브젝트 구현

---
+++ GhostAdventureProject/Assets/01.Scripts/Object/MemoryObject/Ch4_MemoryObject_Mirror.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _01.Scripts.Object.MemoryObject
+{
+    public class Ch4_MemoryObject_Mirror : MemoryFragment {
+        override protected void Start() {
+            base.Start();
+            isScannable = true;
+        }
+    }
+}
