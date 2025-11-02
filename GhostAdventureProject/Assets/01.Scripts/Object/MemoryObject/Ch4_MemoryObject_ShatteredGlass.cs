21839f6227f83c2886aab60dad6dc1f4d1883bf5
Bangeunseong <bangeunseong@naver.com>
2025-11-03 00:26:49 +0900

    [feat] 모든 기억 조각 오브젝트 구현

---
+++ GhostAdventureProject/Assets/01.Scripts/Object/MemoryObject/Ch4_MemoryObject_ShatteredGlass.cs
@@ -0,0 +1,9 @@
+﻿namespace _01.Scripts.Object.MemoryObject
+{
+    public class Ch4_MemoryObject_ShatteredGlass : MemoryFragment {
+        public override void SetScannable(bool value) {
+            base.SetScannable(value);
+            gameObject.SetActive(value);
+        }
+    }
+}
--- GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/PO_Animal/Ch4_Spider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _01.Scripts.Object.PossessableObject.PO_Animal
-{
-    public class Ch4_Spider : BasePossessable
-    {
-        
-    }
-}
