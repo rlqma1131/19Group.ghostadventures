f69ca42a7cf269afb0397918f0d065798bf37042
hbks0224 <hbks1234@gmail.com>
2025-11-04 20:41:13 +0900

    feat : 기억조각 4번 본 이후 기억조각 5번 스캔 가능하게 스크립트 수정

--- GhostAdventureProject/Assets/01.Scripts/Object/MemoryObject/Ch4_MemoryObject_Mirror.cs
+++ GhostAdventureProject/Assets/01.Scripts/Object/MemoryObject/Ch4_MemoryObject_Mirror.cs
@@ -5,7 +5,13 @@ namespace _01.Scripts.Object.MemoryObject
     public class Ch4_MemoryObject_Mirror : MemoryFragment {
         override protected void Start() {
             base.Start();
-            isScannable = true;
+            isScannable = false;
         }
+
+
+        public void EnableScanning()
+        {
+            isScannable = true;
+        } 
     }
 }
